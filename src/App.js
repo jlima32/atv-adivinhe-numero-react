f322f1b8a630388981cabb03280acb7cf0a46229
Juan Lima <juangustavo@gmail.com>
2024-09-07 10:25:57 -0300

    feat: biblioteca random-int para gerar número aleatório

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import Header from './components/header/header';
 import Palpite from './components/palpite/palpite';
+import randomInteger from 'random-int';
 
 function App() {
+
+
+  let inicial, final;
+
+  function SortearNumero(){
+    inicial = 0;
+    final = 10;
+    
+    const numeroAleatorio = randomInteger(inicial,final);
+    return numeroAleatorio;
+    
+  }
+
+  
   return (
     <div className='container'>
+
+      <SortearNumero />
       <Header />
       <Palpite />
-      
     </div>
   );
+  
 }
 
 export default App;
--- src/components/palpite/palpite.js
+++ src/components/palpite/palpite.js
@@ -1,10 +1,12 @@
 import "./palpite.css"
 
 export default function Palpite(){
+    
     return <>
         <div className="palpite">
-            <input type="text" name="palpite" className="palpiteText" placeholder="Digite um número entre 0 e 100"/>
+            <input type="text" name="palpite" className="palpiteText" placeholder="Digite um número entre 0 e 10" />
             <button type="submit" className="btnPalpitar">CHUTAR</button>
         </div>
     </>
+    
 }
